7dbfbfa76cc9cc065c98557f64491fb8e25bc9bd
helixin125 <992489930@qq.com>
2019-10-05 10:02:46 +0800

    ADD

--- src/main/java/com/helx/controller/MemberController.java
+++ src/main/java/com/helx/controller/MemberController.java
@@ -1,13 +1,18 @@
 package com.helx.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MemberController {
 
+	@Value("${server.port}")
+	String port;
+	
 	@RequestMapping("/getMsg")
 	public String getMsg() {
-		return "注册到Eureka";
+		System.out.println("注册到Eureka,port:"+port);
+		return "注册到Eureka,port:"+port;
 	}
 }
